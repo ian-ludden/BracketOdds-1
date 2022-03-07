246e6fce773389bffb33e4e7c0f292b42ca0e321
ian-ludden <iludden94@gmail.com>
2022-03-07 10:51:32 -0600

    Use updated api to fetch multiple brackets at once.

--- fetch_bracket.py
+++ fetch_bracket.py
@@ -16,22 +16,23 @@ Returns:
 ===============
     the generated bracket as a length-63 string of 0s and 1s
 """
-def fetch_bracket(type="men", sfn=None):
-    bracketodds_url = "https://bracketodds.cs.illinois.edu/api/v1/bracket?type=" + type
+def fetch_brackets(type="men", sfn=None, count=1):
+    bracketodds_url = "https://bracketodds.cs.illinois.edu/api/v1/bracket/multiplebrackets?type=" + type
+    bracketodds_url += "&count=" + str(count)
     if sfn:
         bracketodds_url += "&sfn=" + sfn
 
     bracketodds_response = requests.get(bracketodds_url)
     data = json.loads(bracketodds_response.text)
-    # print(data)
-    return data['bitstring']
+    return data['bitstrings']
 
 
 if __name__ == '__main__':
     # TODO: Add command-line arguments for 
     # how many brackets per pool and 
     # how many pools to fetch. 
-    brackets_per_pool = 100000
+    brackets_per_request = 200
+    requests_per_pool = 500
     num_pools = 10
 
     for type in ["men"]:
@@ -39,12 +40,12 @@ if __name__ == '__main__':
         for sfn in [None, "f4a", "e8"]:
             print(sfn)
             for pool_index in range(num_pools):
-                filename = "bracket_pools/prefix0_bracket_pool_{0}_{1}_{2:02d}.txt".format(type, sfn if sfn else "default", pool_index)
+                filename = "bracket_pools/bracket_pool_{0}_{1}_{2:02d}.txt".format(type, sfn if sfn else "default", pool_index)
                 with open(filename, 'w') as out_f:
-                    for bracket_index in range(brackets_per_pool):
-                        bitstring = fetch_bracket(type=type, sfn=sfn)
-                        prefix0_bitstring = "0" + bitstring
-                        integer_val = int(prefix0_bitstring, 2)
-                        out_f.write("{:016X}\n".format(integer_val))
+                    for request_index in range(requests_per_pool):
+                        bitstrings = fetch_brackets(type=type, sfn=sfn, count=brackets_per_request)
+                        for bitstring in bitstrings:
+                            integer_val = int(bitstring, 2)
+                            out_f.write("{:016X}\n".format(integer_val))
         print()
                     

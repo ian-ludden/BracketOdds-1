a29a070a595c2ea2cff299ce5eb7d8f32a2e39ab
Ian G. Ludden <iludden94@gmail.com>
2024-03-14 10:47:50 -0400

    Update men's alpha values and sample brackets before 2024 tournament.

--- fetch_bracket.py
+++ fetch_bracket.py
@@ -8,22 +8,39 @@ if __name__ == '__main__':
     # TODO: Add command-line arguments for 
     # how many brackets per pool and 
     # how many pools to fetch. 
-    brackets_per_pool = 10
+    brackets_per_pool = 100000
     num_pools = 10
 
-    for type in ["men"]:
-        print(type)
+    type_str = "men"
+    print(type_str)
+    
+    for sfn_name in [None, "f4a", "e8"]:
+        print(sfn_name)
+        sfn = None if (not sfn_name) else (F4_A() if sfn_name == "f4a" else E_8())
         
-        for sfn_name in [None, "f4a", "e8"]:
-            print(sfn_name)
-            sfn = None if (not sfn_name) else (F4_A() if sfn_name == "f4a" else E_8())
-            
-            for pool_index in range(num_pools):
-                filename = "bracket_pools/bracket_pool_{0}_{1}_{2:02d}.txt".format(type, sfn_name if sfn_name else "default", pool_index)
-                with open(filename, 'w') as out_f:
-                    for bracket_index in range(brackets_per_pool):
-                        bitstring = getNewBracketBitstring(BracketType(type), sampling_fn=sfn)
-                        integer_val = int(bitstring, 2)
-                        out_f.write("{:016X}\n".format(integer_val))
-        print()
-                    
+        for pool_index in range(num_pools):
+            filename = "bracket_pools/bracket_pool_{0}_{1}_{2:02d}.txt".format(type_str, sfn_name if sfn_name else "default", pool_index)
+            with open(filename, 'w') as out_f:
+                for bracket_index in range(brackets_per_pool):
+                    bitstring = getNewBracketBitstring(BracketType(type_str), sampling_fn=sfn)
+                    integer_val = int(bitstring, 2)
+                    out_f.write("{:016X}\n".format(integer_val))
+    print()
+
+
+    type_str = "women"
+    print(type_str)
+
+    for sfn_name in [None]:
+        print(sfn_name)
+        sfn = None if (not sfn_name) else (F4_A() if sfn_name == "f4a" else E_8())
+        
+        for pool_index in range(num_pools):
+            filename = "bracket_pools/bracket_pool_{0}_{1}_{2:02d}.txt".format(type_str, sfn_name if sfn_name else "default", pool_index)
+            with open(filename, 'w') as out_f:
+                for bracket_index in range(brackets_per_pool):
+                    bitstring = getNewBracketBitstring(BracketType(type_str), sampling_fn=sfn)
+                    integer_val = int(bitstring, 2)
+                    out_f.write("{:016X}\n".format(integer_val))
+    print()
+             
